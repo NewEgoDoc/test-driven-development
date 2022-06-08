0d2f718f3fb5239fe8ba99f8ead1ba58d5718fa3
Juhan <juhan211@naver.com>
2022-06-08 18:45:34 +0900

    5장. 솔직히 말하자면 - intellij 도움이 아닌 일단 구현된 것들만 적용!

--- src/main/java/com/example/tdd/domain/Franc.java
+++ src/main/java/com/example/tdd/domain/Franc.java
@@ -15,14 +15,7 @@ public class Franc {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
         Franc franc = (Franc) o;
         return amount == franc.amount;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount);
     }
 }
