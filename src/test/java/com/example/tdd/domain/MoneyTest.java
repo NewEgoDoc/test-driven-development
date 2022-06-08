022b322f86963fa3e426fdba993856cb475c02e2
Juhan <juhan211@naver.com>
2022-06-08 19:48:36 +0900

    8장. 객체 만들기 - 팩토리 메서드를 사용하여 테스트 코드 수정

--- src/test/java/com/example/tdd/domain/MoneyTest.java
+++ src/test/java/com/example/tdd/domain/MoneyTest.java
@@ -9,20 +9,20 @@ public class MoneyTest {
 
     @Test
     public void testMultiplication(){
-        Dollar five = Dollar.dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15),five.times(3));
+        Money five = Dollar.dollar(5);
+        assertEquals(Dollar.dollar(10), five.times(2));
+        assertEquals(Dollar.dollar(15),five.times(3));
     }
 
     @Test
     public void testEquality(){
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(Dollar.dollar(5).equals(Dollar.dollar(5)));
+        assertFalse(Dollar.dollar(5).equals(Dollar.dollar(6)));
 
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
 
-        assertFalse(new Franc(5).equals(new Dollar(5)));
+        assertFalse(new Franc(5).equals(Dollar.dollar(5)));
     }
 
     @Test
