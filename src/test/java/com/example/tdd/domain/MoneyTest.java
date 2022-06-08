f3b03f95e91b13832b351a1a683654e8c76c98bf
Juhan <juhan211@naver.com>
2022-06-08 17:11:30 +0900

    2장. 타락한 객체 - 실패하는 test code 만들기

--- src/test/java/com/example/tdd/domain/MoneyTest.java
+++ src/test/java/com/example/tdd/domain/MoneyTest.java
@@ -11,6 +11,8 @@ public class MoneyTest {
     public void testMultiplication(){
         Dollar five = new Dollar(5);
         five.times(2);
-        Assertions.assertEquals(10, five.amount);
+        assertEquals(10, five.amount);
+        five.times(3);
+        assertEquals(15,five.amount);
     }
 }
