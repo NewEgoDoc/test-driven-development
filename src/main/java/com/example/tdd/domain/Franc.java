85c2c3a9fe1fc73e4855bc59ef7b8d908b263985
Juhan <juhan211@naver.com>
2022-06-08 19:19:30 +0900

    6장. 돌아온 '모두를 위한 평등' - Franc도 똑같이 변환!

--- src/main/java/com/example/tdd/domain/Franc.java
+++ src/main/java/com/example/tdd/domain/Franc.java
@@ -2,20 +2,12 @@ package com.example.tdd.domain;
 
 import java.util.Objects;
 
-public class Franc {
-    private int amount;
-
+public class Franc extends Money{
     public Franc(int amount) {
         this.amount = amount;
     }
 
     public Franc times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        Franc franc = (Franc) o;
-        return amount == franc.amount;
     }
 }
