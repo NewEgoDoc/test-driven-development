5526e8d0fcfe08afa56bd259a79aea1fa1bdbe46
Juhan <juhan211@naver.com>
2022-06-08 20:46:10 +0900

    9장. 우리가 사는 시간 - Money에 currency() 추상메서드를 선언

--- src/main/java/com/example/tdd/domain/Money.java
+++ src/main/java/com/example/tdd/domain/Money.java
@@ -4,7 +4,18 @@ import java.util.Objects;
 
 public abstract class Money {
     protected int amount;
+    protected String currency;
+
+    public static Money dollar(int i) {
+        return null;
+    }
+
+    public static Money franc(int i) {
+        return null;
+    }
+
     abstract Money times(int multiplier);
+    abstract String currency();
 
     @Override
     public boolean equals(Object o) {
