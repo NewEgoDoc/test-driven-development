779a3065e5c16398a497fb5e277967f8a14c8869
Juhan <juhan211@naver.com>
2022-06-09 20:12:37 +0900

    12장. 드디어, 더하기 - testSimpleAddition 테스트 추가후 필요한 plus() 함수 추가구성!

--- src/test/java/com/example/tdd/domain/MoneyTest.java
+++ src/test/java/com/example/tdd/domain/MoneyTest.java
@@ -33,4 +33,9 @@ public class MoneyTest {
         assertTrue(new Money(10,"CHF").equals(
                 new Franc(10,"CHF")));
     }
+
+    @Test
+    public void testSimpleAddition(){
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+    }
 }
