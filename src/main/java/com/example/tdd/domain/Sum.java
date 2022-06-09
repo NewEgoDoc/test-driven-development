0ab84bd2ff1b465e3ebb2aea7e499250f8e14ee2
Juhan <juhan211@naver.com>
2022-06-09 22:58:19 +0900

    12장. 드디어, 더하기 - testPlusReturnSum 테스트를 위한 클래스와 인수들을 만들고 Money의 plus 함수를 수정 하였다

---
+++ src/main/java/com/example/tdd/domain/Sum.java
@@ -0,0 +1,13 @@
+package com.example.tdd.domain;
+
+public class Sum implements Expression{
+    Money augend;
+    Money addend;
+
+
+    public Sum(Money augend, Money addend) {
+        this.augend = augend;
+        this.addend = addend;
+    }
+
+}
