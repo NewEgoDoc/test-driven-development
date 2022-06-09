9dd9541622bfdc3e901396105713bd8453697192
Juhan <juhan211@naver.com>
2022-06-09 19:50:27 +0900

    11장. 모든 악의 근원 - 팩토리 메서드 Money 반환

--- src/main/java/com/example/tdd/domain/Dollar.java
+++ src/main/java/com/example/tdd/domain/Dollar.java
@@ -1,9 +1,6 @@
 package com.example.tdd.domain;
 
-import java.util.Objects;
-
 public class Dollar extends Money{
-
     public Dollar(int amount, String currency) {
         super(amount,currency);
     }
