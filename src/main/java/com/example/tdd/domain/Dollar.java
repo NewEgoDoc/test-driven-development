0d080ac9d0d2906212d4d87cb8df2de8e6a4503a
Juhan <juhan211@naver.com>
2022-06-09 19:39:03 +0900

    10장. 흥미로운 시간

--- src/main/java/com/example/tdd/domain/Dollar.java
+++ src/main/java/com/example/tdd/domain/Dollar.java
@@ -7,13 +7,4 @@ public class Dollar extends Money{
     public Dollar(int amount, String currency) {
         super(amount,currency);
     }
-
-    public static Dollar dollar(int amount) {
-        return new Dollar(amount,"USD");
-    }
-
-    @Override
-    String currency() {
-        return currency;
-    }
 }
--- src/main/java/com/example/tdd/domain/Franc.java
+++ src/main/java/com/example/tdd/domain/Franc.java
@@ -7,13 +7,4 @@ public class Franc extends Money{
     public Franc(int amount, String currency) {
         super(amount,currency);
     }
-
-    public static Franc franc(int amount) {
-        return new Franc(amount, null);
-    }
-
-    @Override
-    String currency() {
-        return currency;
-    }
 }
