aa63b6fd38656726bb34016dd288fda8b34494dd
Juhan <juhan211@naver.com>
2022-06-10 22:26:45 +0900

    14장. 바꾸기 - 환율 설정

--- src/main/java/com/example/tdd/domain/Bank.java
+++ src/main/java/com/example/tdd/domain/Bank.java
@@ -1,6 +1,10 @@
 package com.example.tdd.domain;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable rates = new Hashtable();
 
     public Money reduce(Expression source, String to) {
         return source.reduce(this, to);
@@ -11,6 +15,7 @@ public class Bank {
         return rate;
     }
 
-    public void addRate(String chf, String usd, int i) {
+    public void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
     }
 }
