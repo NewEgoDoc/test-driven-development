53c4bac1dfd561a60124e1973dec8717e208d715
Juhan <juhan211@naver.com>
2022-06-13 19:02:01 +0900

    15장. 서로 다른 통화 더하기 - $5 + 10CHF 테스트 추가

--- src/main/java/com/example/tdd/TestDrivenDevelopmentApplication.java
+++ src/main/java/com/example/tdd/TestDrivenDevelopmentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestDrivenDesignApplication {
+public class TestDrivenDevelopmentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestDrivenDesignApplication.class, args);
+        SpringApplication.run(TestDrivenDevelopmentApplication.class, args);
     }
 
 }
