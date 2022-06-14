9569379266d5e4ea2923f9e9b3c1aaebccef95ee
Juhan <juhan211@naver.com>
2022-06-14 22:39:31 +0900

    25장. 테스트 주도 개발 패턴 - (3) 명백한 데이터를 사용하라 - (클래스와 메소드를 구성하지는 않았지만) 이 거래에는 수수료 1.5%가 붙는다. USD에서 GBP로 교환하는 환율이 2:1이라면 $100를 환전하려면 50GBP-1.5% = 49.25GBP여야 한다. 를 테스트로 작성

--- src/test/java/com/example/tdd/pattern/TestPattern.java
+++ src/test/java/com/example/tdd/pattern/TestPattern.java
@@ -1,6 +1,8 @@
 package com.example.tdd.pattern;
 
 
+import com.example.tdd.domain.Bank;
+import com.example.tdd.domain.Money;
 import org.apache.catalina.Server;
 import org.apache.tomcat.jni.Buffer;
 import org.junit.jupiter.api.Test;
@@ -17,5 +19,14 @@ public class TestPattern {
         Buffer reply = reader.contents();
         assertTrue(reader.isClosed());
         assertEquals("abc", reply.contents());
+    }
+
+    @Test
+    public void testClearData(){
+        Bank bank = new Bank();
+        bank.addRate("USD", "GBP", STANDARD_RATE);
+        bank.commission(STANDARD_COMMISSION);
+        Money result = bank.convert(new Note(100, "USD"), "GBP");
+        assertEquals(new Note(49.25, "GBP"), result);
     }
 }
