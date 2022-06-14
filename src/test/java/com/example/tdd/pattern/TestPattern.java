121b25f8b784c38ea1c0c2c6a13c90b1342ce1cc
Juhan <juhan211@naver.com>
2022-06-14 22:30:16 +0900

    25장. 테스트 주도 개발 패턴 - (1)테스트 우선 테스트부터 작성하고 구현하자!

---
+++ src/test/java/com/example/tdd/pattern/TestPattern.java
@@ -0,0 +1,18 @@
+package com.example.tdd.pattern;
+
+import org.apache.catalina.Server;
+import org.apache.tomcat.jni.Buffer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.rsocket.RSocketProperties;
+
+import java.net.Socket;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class TestPattern {
+
+    @Test
+    public void testsocket(){
+
+    }
+}
